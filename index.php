4a62a72f4edc70e52e88d53a984f9a651799a5f6
Ivan Ostojic <ivan.ostojic@sum.ba>
2022-12-07 09:56:28 +0100

    404 not found

--- index.php
+++ index.php
@@ -3,11 +3,27 @@
 require 'functions.php';
 
 $uri = $_SERVER['REQUEST_URI'];
+$parsedUrl = parse_url($uri)['path'];
 
-if ($uri == '/') {
-    require 'controllers/index.php';
-} else if ($uri == '/about') {
-    require 'controllers/about.php';
-} else if ($uri == '/contact') {
-    require 'controllers/contact.php';
+//if ($parsedUrl == '/') {
+//    require 'controllers/index.php';
+//} else if ($parsedUrl == '/about') {
+//    require 'controllers/about.php';
+//} else if ($parsedUrl == '/contact') {
+//    require 'controllers/contact.php';
+//}
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+if (array_key_exists($parsedUrl, $routes)) {
+    require $routes[$parsedUrl];
+} else {
+    http_response_code(404);
+
+    require ('views/not_found.php');
+    die();
 }
---
+++ views/not_found.php
@@ -0,0 +1,9 @@
+<html>
+<?php require('partials/head.view.php') ?>
+<?php require('partials/navbar.view.php') ?>
+<div class="d-flex flex-column justify-content-center align-items-center p-5">
+    <h1>Not found</h1>
+    <h2>404</h2>
+</div>
+
+<?php require('partials/footer.view.php') ?>
