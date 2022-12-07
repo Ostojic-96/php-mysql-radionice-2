125a41f4c719d0c34f3d308f2bf7c2737cf01c23
Ivan Ostojic <ivan.ostojic@sum.ba>
2022-12-07 12:17:37 +0100

    spajanje na bazu podataka

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
 
 require 'functions.php';
 require 'router.php';
+
+// Data source name
+//$dsn = 'mysql:host=localhost;dbname=moviedb';
+//$pdo = new PDO($dsn, 'osta', '123456');
+//
+//$statement = $pdo->prepare("select * from movies");
+//$statement->execute();
+//
+//$movies = $statement->fetchAll(PDO::FETCH_ASSOC);
+//foreach ($movies as $movie) {
+//    echo $movie['title'];
+//    echo '<br>';
+//}
+
+
